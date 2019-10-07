87fce285bee913a83078757d4325d32ccb74a23d
s154666 <s154666@student.dtu.dk>
2019-10-07 17:01:12 +0200

    Added the 'demo' project which is our current website working with Spring Boot JST

---
+++ demo/src/main/java/com/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class HelloController 
+{
+    @GetMapping({"/", "/index"})
+    public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) 
+    {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
