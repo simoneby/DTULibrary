5c92b34d1309aa2c0d06b9de8925969de07d00b7
Simone Bystadhagen <simonby@stud.ntnu.no>
2019-11-12 14:37:21 +0100

    add working GET method for ticket

--- Mazemap/src/main/java/com/controllers/LoginController.java
+++ Mazemap/src/main/java/com/controllers/LoginController.java
@@ -13,6 +13,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.*;
+import java.lang.*;
+import org.springframework.*;
+// import com.controllers.RedirectController.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SessionAttributes("user")
 @Controller
@@ -76,48 +87,99 @@ public class LoginController {
 	// }
 	// }
 	//@ModelAttribute("user")
-	@RequestMapping(value = "/signin", method = RequestMethod.POST)
-	public String signin(
-			@RequestParam(value = "email", required = true, defaultValue = "") String email,
-			@RequestParam(value = "password", required = false, defaultValue = "") String password) {
-		/*
-		 * if(roleRepository.findAll()==null ||
-		 * !roleRepository.findAll().iterator().hasNext()) { roleRepository.save(new
-		 * Role("Student")); roleRepository.save(new Role("Faculty"));
-		 * roleRepository.save(new Role("Library Staff")); }
-		 */
+//	@RequestMapping(value = "/signin", method = RequestMethod.POST)
+//	public String signin(
+//			@RequestParam(value = "email", required = true, defaultValue = "") String email,
+//			@RequestParam(value = "password", required = false, defaultValue = "") String password) {
+//		/*
+//		 * if(roleRepository.findAll()==null ||
+//		 * !roleRepository.findAll().iterator().hasNext()) { roleRepository.save(new
+//		 * Role("Student")); roleRepository.save(new Role("Faculty"));
+//		 * roleRepository.save(new Role("Library Staff")); }
+//		 */
+//
+//		// check DTU inside sign in
+//		if (email == null)
+//			return "Email is null";
+//		boolean succesfulDTUSignIn = true;
+//		boolean login = false;
+//		if (succesfulDTUSignIn) {
+//			if (userRepository.findUsersByEmail(email).isEmpty()) {
+//				User entity = new User();
+//				entity.setEmail(email);
+//				entity.setName("some name");
+//				entity.addRole(roleRepository.findAll().iterator().next());
+//				userRepository.save(entity);
+//				this.user = entity;
+//				login = true;
+//			} else {
+//				this.user= userRepository.findUserByEmail(email);
+//				login = true;
+//			}
+//		} else {
+//			if (!userRepository.findUsersByEmail(email).isEmpty()) {
+//				this.user = userRepository.findUserByEmail(email);
+//				login = true;
+//			}
+//		}
+//		if(login)
+//		{
+//			setUpUserForm();
+//			return loginsuccesful(this.user.getEmail());
+//		}
+//		return "Login Failed!";
+//	}
 
-		// check DTU inside sign in
-		if (email == null)
-			return "Email is null";
-		boolean succesfulDTUSignIn = true;
-		boolean login = false;
-		if (succesfulDTUSignIn) {
-			if (userRepository.findUsersByEmail(email).isEmpty()) {
-				User entity = new User();
-				entity.setEmail(email);
-				entity.setName("some name");
-				entity.addRole(roleRepository.findAll().iterator().next());
-				userRepository.save(entity);
-				this.user = entity;
-				login = true;
-			} else {
-				this.user= userRepository.findUserByEmail(email);
-				login = true;
-			}
-		} else {
-			if (!userRepository.findUsersByEmail(email).isEmpty()) {
-				this.user = userRepository.findUserByEmail(email);
-				login = true;
-			}
-		}
-		if(login)
-		{
-			setUpUserForm();
-			return loginsuccesful(this.user.getEmail());
-		}
-		return "Login Failed!";
+	@RequestMapping(value="/signin",method=RequestMethod.POST)
+	public RedirectView login(HttpServletResponse HttpServletResponse)
+	{
+		// REROUTE TO ADDESS:
+		// https://auth.dtu.dk/dtu/?service=https%3A%2F%2Fse2%2Dwebapp05%2Ecompute%2Edtu%2Edk%3A8080%2F
+		RedirectView redirectView = new RedirectView();
+
+		// FOR REALSIES
+		redirectView.setUrl("https://auth.dtu.dk/dtu/?service=https%3A%2F%2Fse2%2Dwebapp05%2Ecompute%2Edtu%2Edk%3A8080%2Fredirect%2F");
+
+		// LOCAL STUFF
+		// redirectView.setUrl("https://auth.dtu.dk/dtu/?service=%5c%5clocalhost%3A8080");//;%2Fredirect%2F");
+
+		return redirectView;
 	}
+
+	// @author s154666
+	@RequestMapping(value="/redirect", method=RequestMethod.GET)
+	@ResponseBody
+	public String redirect(@RequestParam("ticket") String ticket) {
+		return "This is the ticket:" + ticket;
+	}
+
+
+
+	//{
+
+		// USER WILL RETURN WITH URL
+		// https://se2-webapp05.compute.dtu.dk:8080?ticket=[TICKET]
+		// WHERE TICKET IS USED TO GET DATA FROM CAMPUSNET ( I THINK ??? )
+
+		// ON SUCCESSFUL LOGIN:
+		// DTU CAS RESPONDS WITH
+		// yes
+		// [USERNAME] (NOT THE USER'S NAME BUT THE USERNAME IN CAS)
+
+
+		// ON UNSUCCESSFUL LOGIN:
+		// DTU CAS RESPONDS WITH
+		// no
+
+
+
+	//	return "Grats you've been logged in!\t";
+	//}
+
+
+
+
+
 	@GetMapping(path="/loginsuccesful")
     public String loginsuccesful(@RequestParam(value="name", required=false, defaultValue="World") String name) {
         return "loginsuccesful";
