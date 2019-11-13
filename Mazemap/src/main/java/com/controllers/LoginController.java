08a937a932dd42067240ecc3284a00eccb81e6e4
s154666 <s154666@student.dtu.dk>
2019-11-13 14:58:27 +0100

    Replace all

--- Mazemap/src/main/java/com/controllers/LoginController.java
+++ Mazemap/src/main/java/com/controllers/LoginController.java
@@ -172,7 +172,7 @@ public class LoginController {
 			InputStream in = con.getInputStream();
 			String encoding = con.getContentEncoding();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
 			encoding = encoding == null ? "UTF-8" : encoding;
-			studentnr = IOUtils.toString(in, encoding).replace(" ","");
+			studentnr = IOUtils.toString(in, encoding).replaceAll("\\s","");
 
 			try 
 			{
