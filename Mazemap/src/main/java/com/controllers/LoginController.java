51f795e1183297d1cc1447e64168dfdd81f77275
Simone Bystadhagen <simonby@stud.ntnu.no>
2019-11-13 15:07:24 +0100

    fix small

--- Mazemap/src/main/java/com/controllers/LoginController.java
+++ Mazemap/src/main/java/com/controllers/LoginController.java
@@ -171,8 +171,9 @@ public class LoginController {
 			URLConnection con = url.openConnection();
 			InputStream in = con.getInputStream();
 			String encoding = con.getContentEncoding();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
-			encoding = encoding == null ? "UTF-8" : encoding;
-			studentnr = IOUtils.toString(in, encoding).replaceAll("\\s","");
+			//encoding = encoding == null ? "UTF-8" : encoding;
+			studentnr = IOUtils.toString(in, "UTF-8").replaceAll("\\s","");
+
 
 			try 
 			{
