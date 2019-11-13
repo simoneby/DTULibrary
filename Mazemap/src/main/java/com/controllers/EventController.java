f9e4cd1833d77f15f89ee39ecde6ce2fe8ab67cf
Kasper Jensen <s183051@win.dtu.dk>
2019-11-13 11:34:27 +0100

    Added backend for show events

--- Mazemap/src/main/java/com/controllers/EventController.java
+++ Mazemap/src/main/java/com/controllers/EventController.java
@@ -1,11 +1,28 @@
 package com.controllers;
+import com.models.*;
 //import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+
 @RestController
+@RequestMapping(path = "/events")
 public class EventController 
 {
+	@Autowired
+	private EventRepository eventRepository;
+	@RequestMapping(value = "/eventdata", method = RequestMethod.GET)
+	  public ArrayList<Event> getEventData(
+	      @RequestParam(name = "userid", required = false, defaultValue = "0") short level) {
+	    // ArrayList<SensorData> sensorData = new ArrayList<SensorData>();
+		ArrayList<Event> events = new ArrayList<Event>();
+		eventRepository.findAll().forEach(events::add);
+	    
+	    	    return events;
+	  }
+
     @RequestMapping({"/otherevent"})
     public String event(@RequestParam(value="name", required=false, defaultValue="World") String name) 
     {
