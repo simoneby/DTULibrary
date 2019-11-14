f620c7dca8c6dce3d8335e1044d416010cfbcd1a
s154666 <s154666@student.dtu.dk>
2019-11-14 22:23:02 +0100

    Whoopsie

--- Mazemap/src/main/java/com/repositories/FilteredUserRepository.java
+++ Mazemap/src/main/java/com/repositories/FilteredUserRepository.java
@@ -9,7 +9,4 @@ public interface FilteredUserRepository extends UserRepository{
 	  List<User> findUsersByEmail(String email);
 	  User findUserByEmail(String email);
 	  User findUserByStudentnr(String studentnr);
-	  // @Modifying
-	  // @Query("update User u set u.name = ?1 where u.studentnr = ?2")
-	  int setFixedNameFor(String name, String studentnr);
 }
