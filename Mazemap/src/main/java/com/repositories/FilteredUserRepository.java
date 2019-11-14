0df2a50254567aa22f9cbaf8907e380b3c38935e
s154666 <s154666@student.dtu.dk>
2019-11-14 22:07:23 +0100

    asd

--- Mazemap/src/main/java/com/repositories/FilteredUserRepository.java
+++ Mazemap/src/main/java/com/repositories/FilteredUserRepository.java
@@ -9,7 +9,7 @@ public interface FilteredUserRepository extends UserRepository{
 	  List<User> findUsersByEmail(String email);
 	  User findUserByEmail(String email);
 	  User findUserByStudentnr(String studentnr);
-	  @Modifying
-	  @Query("update User u set u.name = ?1 where u.studentnr = ?2")
+	  // @Modifying
+	  // @Query("update User u set u.name = ?1 where u.studentnr = ?2")
 	  int setFixedNameFor(String name, String studentnr);
 }
