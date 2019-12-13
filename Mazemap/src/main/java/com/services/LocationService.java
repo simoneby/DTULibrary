f5a99f88e508337284bb12673c2d64b713147b23
Xin Lu Guo <patriciaguo@patricias-mbp.eduroam.wireless.dtu.dk>
2019-12-13 19:14:54 +0100

    tests a work in progress

---
+++ Mazemap/src/main/java/com/services/LocationService.java
@@ -0,0 +1,62 @@
+package com.services;
+
+import com.repositories.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.models.*;
+
+//@Author s191545
+@Component
+public class LocationService {
+	@Autowired
+    private LocationRepository locationRepository;
+    @Autowired
+    private FilteredUserRepository userRepository;
+
+
+    public Set<User> addMessage(String userEmail) {
+        if (location != null && cu != null) {
+            try {
+            	if (loc!= null){
+					loc.setLocationMessage(location.getLocationMessage());
+	            	loc.setCoordinateX(location.getCoordinateX());
+	            	loc.setCoordinateY(location.getCoordinateY());
+            		locationRepository.save(loc);
+            	}
+            	else{
+            	location.setUser(cu);  
+  	
+            	locationRepository.save(location);
+            	}
+                return String.format("Your location was saved %s",location.getLocationMessage());
+            
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "There was an error and location could not be saved";
+            }
+        }
+        return userRepository.findUserByEmail(userEmail).getFriends();
+    }
+
+    public ArrayList<LocationOfUsers> getAllLocations() {
+        ArrayList<LocationOfUsers> locations = new ArrayList<LocationOfUsers>();
+        locationRepository.findAll().forEach(locations::add);
+        return locations;
+
+    public Set<User> getAllFriends(String userEmail) {
+        if (userEmail == null) {
+            try {
+                throw new Exception("User email undefined");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new HashSet<User>();
+            }
+        }
+        return userRepository.findUserByEmail(userEmail).getFriends();
+    }
+
+}
---
+++ Mazemap/src/test/java/com/example/demo/LocationUnitTest.java
@@ -0,0 +1,148 @@
+// package com.example.demo;
+
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ContextConfiguration;
+// import org.springframework.test.context.TestPropertySource;
+// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+// import org.springframework.test.context.junit4.SpringRunner;
+
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.List;
+// import org.junit.After;
+// import org.junit.Assert;
+// import org.junit.Before;
+// import org.junit.BeforeClass;
+// import com.repositories.*;
+// import java.util.Set;
+
+// import com.SpringBootJspApplication;
+// import com.models.*;
+// import com.services.*;
+
+// //run by using cmd mvn -q test
+// @SpringBootTest
+// @RunWith(SpringRunner.class)
+// @org.springframework.transaction.annotation.Transactional
+// // @ContextConfiguration(
+// // classes = {FilteredUserRepository.class,FriendListService.class})
+// @TestPropertySource("/application.properties")
+// public class LocationUnitTest {
+//    static String student_number_format="sxxxxx%s";
+//    static String student_email_format="sxxxxx%s@student.dtu.dk";
+//    @Autowired
+//    FilteredUserRepository userRepository;
+//    @Autowired
+//    FriendListService friendListService;
+//    @Autowired
+//    LocationRepository LocationRepository;
+
+//    //this method is executed before every test case
+//    //use it to create any data you might need / initialize properties for the test
+//    @Before
+//    public void initializeTest() {
+//       deleteCreatedUsers();
+//       HashMap<Integer,User> createdUsers = new HashMap<Integer,User>();
+//       for(int i=1;i<7;i++)
+//       {
+//          String studentNr = String.format(student_number_format,i);
+//          String email = String.format(student_email_format,i);
+//          User user = new User("Name"+i,studentNr,email);
+//          userRepository.save(user);
+//          createdUsers.put(i,user);
+//       }
+//       User friend1 = createdUsers.get(1);
+//       User friend2 = createdUsers.get(2);
+//       User friend3 = createdUsers.get(3);
+//       User friend4 = createdUsers.get(4);
+//       friend1.setFriend(friend2);
+//       friend2.setFriend(friend1);
+//       friend1.setFriend(friend3);
+//       friend4.setFriend(friend1);
+      
+//       userRepository.save(friend1);
+//       userRepository.save(friend2);
+//       userRepository.save(friend3);
+//       userRepository.save(friend4);
+//       // friendListService = new FriendListService();
+//    }
+//    //this method is executed after every test method
+//    //use this to cleanup / remove any data you have created in the before method or in the tests
+//    @After
+//    public void cleanupTest() {
+//       deleteCreatedUsers();
+//    }
+
+//    //test case checking that method getAllFriends returns the proper result
+//    //results need to be used to check user location
+//    @Test
+//    public void testGetAllFriends() {
+//       //execute the method from the service
+//       String userEmail = String.format(student_email_format,1);
+//       Set<User> friends = friendListService.getAllFriends(userEmail);
+//       //declare asserts (conditions that the result of the method must meet for the tests to pass)
+//       Assert.assertNotNull("getAllFriends is null", friends);
+//       Assert.assertTrue(!friends.isEmpty());
+//    }
+
+//    //test case checking that method getSentFriendRequests returns the proper result
+//    @Test
+//    public void testGetAllFriendsLocations() {
+//       String userEmail = String.format(student_email_format,1);
+//       Assert.assertNotNull(friendListService);
+//       Set<User> friends = friendListService.getSentFriendRequests(userEmail);
+//       Assert.assertNotNull("getSentFriendRequests is null", friends);
+//       Assert.assertTrue(!friends.isEmpty());
+//    }
+
+//    //test case checking that method getSentFriendRequests returns the proper result
+//    @Test
+//    public void testAddLocation() {
+//       //setting up variable before executing the method
+//       String userEmail1 = String.format(student_email_format,1);
+//       String userEmail2 = String.format(student_email_format,5);
+//       //getting some information about the object before execution
+//       Set<User> sentFriendRequests_initial = userRepository.findUserByEmail(userEmail1).getSentFriendRequests();
+//       Set<User> receivedFriendRequests_initial = userRepository.findUserByEmail(userEmail2).getReceivedFriendRequests();      
+//       //executing the method
+//       String result = friendListService.addFriend(userEmail1,userEmail2);
+//       //getting info about the objects after execution
+//       Set<User> sentFriendRequests_modified = userRepository.findUserByEmail(userEmail1).getSentFriendRequests();
+//       Set<User> receivedFriendRequests_modified = userRepository.findUserByEmail(userEmail2).getReceivedFriendRequests();      
+     
+//       Assert.assertNotNull(result);
+//       Assert.assertNotNull(sentFriendRequests_initial);
+//       Assert.assertNotNull(receivedFriendRequests_initial);
+//       Assert.assertNotNull(sentFriendRequests_modified);
+//       Assert.assertNotNull(receivedFriendRequests_modified);
+//       Assert.assertSame((sentFriendRequests_initial.size() + 1), sentFriendRequests_modified.size());
+//       Assert.assertSame((receivedFriendRequests_initial.size() + 1), receivedFriendRequests_modified.size());
+//    }
+//    @Test
+//    public void testBroadcastLocation() {
+//       String userEmail = String.format(student_email_format,1);
+//       Set<User> friends = friendListService.getReceivedFriendRequests(userEmail);
+//       Assert.assertNotNull("getReceivedFriendRequests is null", friends);
+//       System.out.println("Size " + friends.size());
+//       Assert.assertTrue(!friends.isEmpty());
+//    }
+
+//    // private void deleteCreatedUsers()
+//    // {
+//    //    for(int i=1;i<7;i++)
+//    //    {
+//    //       List<User> users = userRepository.findUsersByEmail(String.format(student_email_format, i));
+//    //       for (User user : users) {
+//    //          user.removeAllFriends();
+//    //       }
+//    //    }
+//    //    for(int i=1;i<7;i++)
+//    //    {
+//    //       List<User> users = userRepository.findUsersByEmail(String.format(student_email_format, i));
+//    //       userRepository.deleteAll(users);
+//    //    }
+//    // }
+// }
