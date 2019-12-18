9d66c1b1b09b78a4d2540ee84a1c6ec0e3c791fa
Laura Petre <laura.p1012@gmail.com>
2019-12-18 15:36:53 +0100

    small fix

--- Mazemap/src/test/java/com/example/demo/FriendListTest.java
+++ Mazemap/src/test/java/com/example/demo/FriendListTest.java
@@ -28,7 +28,7 @@ import com.services.*;
 @RunWith(SpringRunner.class)
 
 @TestPropertySource("/testing.properties")
-public class FirstUnitTest {
+public class FriendListTest {
    static String student_number_format="sxxxxx%s";
    static String student_email_format="sxxxxx%s@student.dtu.dk";
    @Autowired
