6ff8cf3e568554a5fe351650dee677dca9727049
Simone Bystadhagen <simonby@stud.ntnu.no>
2019-12-18 01:56:12 +0100

    fix old controller

--- Mazemap/src/main/java/com/controllers/SurveyController.java
+++ Mazemap/src/main/java/com/controllers/SurveyController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.sql.Date;
 
-
 // @Author: s191772 and s154666
 @RequestMapping("/survey")
 @RestController
@@ -34,9 +33,9 @@ public class SurveyController {
     @Autowired
     private QuestionAnswerRepository questionAnswerRepository;
 
+
     @Autowired
     private SurveyService surveyservice;
-
 
     @PostMapping(value = "/save", headers = "Accept='application/json'")
     public String save(@SessionAttribute("user") User currentUser, @RequestBody Survey survey) {
@@ -45,8 +44,7 @@ public class SurveyController {
     }
 
     @GetMapping(value = "/active")
-    public @ResponseBody
-    Set<Survey> activeSurvey() {
+    public @ResponseBody Set<Survey> activeSurvey() {
         return surveyservice.activeSurvey();
 
     }
@@ -70,10 +68,14 @@ public class SurveyController {
         return surveyRepository.findByCreator(currentUser);
     }
 
+    @GetMapping(value = "/current/answer")
+    public @ResponseBody SurveyAnswer currentSurveyAnswer(@RequestParam int survey_id) {
+        return surveyservice.getCurrentSurveyAnswer(survey_id);
+    }
+
 
     @GetMapping(value = "/question_answers")
-    public @ResponseBody
-    Set<QuestionAnswer> getQuestionAnswers(@RequestParam int question_id) {
+    public @ResponseBody Set<QuestionAnswer> getQuestionAnswers(@RequestParam int question_id) {
         return questionAnswerRepository.findByQuestionId(question_id);
     }
 
