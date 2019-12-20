106fbd32dd25d74dbf43ca88a59fc1dd23a3812b
Kasper <Kasper@DESKTOP-CUDK8OT>
2019-12-20 19:50:34 +0100

    cleaning up surverurl a bit

--- Mazemap/src/main/java/com/helpers/ServerUrl.java
+++ Mazemap/src/main/java/com/helpers/ServerUrl.java
@@ -4,8 +4,8 @@ package com.helpers;
 public class ServerUrl {
 
     public static String baseUrl = "https://se2-webapp05.compute.dtu.dk:8443/mazemap";
-	  //"http://localhost:8443"; 
-    //change to https://se2-webapp05.compute.dtu.dk:8443/mazemap when pushing 
+	//public static String baseUrl ="http://localhost:8080"; 
+    //(internal development note) change to https://se2-webapp05.compute.dtu.dk:8443/mazemap when pushing to master 
 
 
 }
